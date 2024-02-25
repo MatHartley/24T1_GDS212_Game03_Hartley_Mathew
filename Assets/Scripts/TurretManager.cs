0f7609df94e92837c880d3a38599bba09e52cdfe
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2024-02-25 16:39:27 +1100

    25/02/24
    Turrets are firing correctly on cooldown
    Differences between levels of turrets implemented - but the changing of levels has not
    Need to remove existing cooldown sliders when upgrading
    
    NEED TO WORK OUT HOW TO MAKE TURRETS FIRE ON TOUCH PRESS
     - tried OnMouseDown with touch mapped to primary mouse and that didn't work, but does for menu buttons
    - tried adding button functionality to turrets - didn't work
    - tried adding OnScreenButton script to turrets, that didnt work

--- Assets/Scripts/TurretManager.cs
+++ Assets/Scripts/TurretManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretManager : MonoBehaviour
 {
@@ -8,12 +9,116 @@ public class TurretManager : MonoBehaviour
     [SerializeField] private int turretLevel;
 
     [Header("Cooldown Values")]
-    [SerializeField] private int cooldownTime;
+    [SerializeField] private float cooldownOne;
+    [SerializeField] private float cooldownTwo;
+    [SerializeField] private float cooldownThree;
+    private float cooldownTime;
     [SerializeField] private float cooldownCount;
+    [SerializeField] Slider[] sliderSelector;
+    public Slider cooldownSlider;
 
     [Header("Bullet Prefabs")]
+    [SerializeField] private GameObject[] bulletSelector;
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float shotSpeed;
 
-    [Header("Turret Prefabs")]
-    [SerializeField] private GameObject[] turretPrefab;
+    [Header("Turret Sprites")]
+    [SerializeField] private Sprite[] turretSprite;
+
+    [Header("Firepoints")]
+    [SerializeField] private Transform[] firepointSelector;
+    [SerializeField] private Transform firePointA;
+    [SerializeField] private Transform firePointB;
+    private bool swapPoint = true;
+
+    public void Start()
+    {
+        //set initial turret at level 1
+        cooldownSlider = sliderSelector[0];
+        cooldownSlider.gameObject.SetActive(true);
+        gameObject.GetComponent<SpriteRenderer>().sprite = turretSprite[0];
+        firePointA = firepointSelector[0];
+        firePointB = firepointSelector[0];
+        bulletPrefab = bulletSelector[0];
+        cooldownTime = cooldownOne;
+
+        cooldownCount = 0;
+        cooldownSlider.minValue = 0;
+        cooldownSlider.maxValue = cooldownTime;
+    }
+
+    private void Update()
+    {
+        cooldownCount += Time.deltaTime;
+        cooldownSlider.value = cooldownCount;
+
+        //select which slider, slider max, cooldown, firepoints, bullet prefab and sprite to use based on which turret model is used
+        switch (turretLevel)
+        {
+            case 1:
+                cooldownSlider = sliderSelector[0];
+                cooldownSlider.gameObject.SetActive(true);
+                gameObject.GetComponent<SpriteRenderer>().sprite = turretSprite[0];
+                firePointA = firepointSelector[0];
+                firePointB = firepointSelector[0];
+                bulletPrefab = bulletSelector[0];
+                cooldownTime = cooldownOne;
+                cooldownSlider.maxValue = cooldownTime;
+                break;
+            case 2:
+                cooldownSlider = sliderSelector[1];
+                cooldownSlider.gameObject.SetActive(true);
+                gameObject.GetComponent<SpriteRenderer>().sprite = turretSprite[1];
+                firePointA = firepointSelector[1];
+                firePointB = firepointSelector[2];
+                bulletPrefab = bulletSelector[0];
+                cooldownTime = cooldownTwo;
+                cooldownSlider.maxValue = cooldownTime;
+                break;
+            case 3:
+                cooldownSlider = sliderSelector[2];
+                cooldownSlider.gameObject.SetActive(true);
+                gameObject.GetComponent<SpriteRenderer>().sprite = turretSprite[2];
+                firePointA = firepointSelector[3];
+                firePointB = firepointSelector[3];
+                bulletPrefab = bulletSelector[1];
+                cooldownTime = cooldownThree;
+                cooldownSlider.maxValue = cooldownTime;
+                break;
+            default:
+                cooldownSlider = sliderSelector[0];
+                cooldownSlider.gameObject.SetActive(true);
+                gameObject.GetComponent<SpriteRenderer>().sprite = turretSprite[0];
+                firePointA = firepointSelector[0];
+                firePointB = firepointSelector[0];
+                bulletPrefab = bulletSelector[0];
+                cooldownTime = cooldownOne;
+                cooldownSlider.maxValue = cooldownTime;
+                break;
+        }
+
+    //}
+
+    //void OnMouseDown()
+    //{
+        if (cooldownCount >= cooldownTime)
+        {
+            Debug.Log("Firing Cannon");
+            GameObject shot = Instantiate(bulletPrefab) as GameObject;
+
+            if (swapPoint)
+            {
+                shot.transform.position = firePointA.transform.position;
+            }
+            else
+            {
+                shot.transform.position = firePointB.transform.position;
+            }
+
+            shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0, shotSpeed);
+
+            swapPoint = !swapPoint;
+            cooldownCount = 0;
+        }
+    }
 }
